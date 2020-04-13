ddbe55a6ee3b8d07ac5a58bb0268cdeb2d7465f6
sergeliatko <serge@techspokes.com>
2020-04-13 14:03:08 +0200

    Added Select.php

---
+++ src/Select.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace SergeLiatko\FormFields;
+
+use SergeLiatko\HTML\Optgroup as OptgroupTag;
+use SergeLiatko\HTML\Option as OptionTag;
+use SergeLiatko\HTML\Select as SelectTag;
+
+/**
+ * Class Select
+ *
+ * @package SergeLiatko\FormFields
+ */
+class Select extends FormField {
+
+	/**
+	 * @param mixed  $value
+	 * @param string $label
+	 * @param array  $current
+	 */
+	protected function getOptionHTML( &$value, $label = '', array $current = array() ) {
+		if ( is_array( $value ) ) {
+			array_walk( $value, array( $this, 'getOptionHTML' ), $current );
+			$value = OptgroupTag::HTML(
+				array(
+					'label' => $label,
+				),
+				$value
+			);
+		} else {
+			$value = in_array( $value, $current ) ?
+				OptionTag::HTML(
+					array(
+						'value'    => $value,
+						'selected' => 'selected',
+					),
+					$label
+				)
+				: OptionTag::HTML(
+					array(
+						'value' => $value,
+					),
+					$label
+				);
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getInputHTML() {
+		$options = $this->getChoices();
+		array_walk( $options, array( $this, 'getOptionHTML' ), $this->getValue() );
+
+		return SelectTag::HTML(
+			$this->getInputAttrs(),
+			$options
+		);
+	}
+
+}
